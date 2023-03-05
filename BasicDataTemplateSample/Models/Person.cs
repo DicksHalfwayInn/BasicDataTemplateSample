5570b68db4a1b26ed1880744e1f554002d21092f
Schofield, stuart <546wine@gmail.com>
2023-03-05 11:16:42 -0700

    Add project files.

---
+++ BasicDataTemplateSample/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDataTemplateSample.Models
+{
+    public class Person
+    {
+        /// <summary>
+        /// Gets or sets the first name of the person. You can only set this property on init.
+        /// </summary>
+        public string? FirstName { get; init; }
+
+        /// <summary>
+        /// Gets or sets the last name of the person. You can only set this property on init.
+        /// </summary>
+        public string? LastName { get; init; }
+
+        /// <summary>
+        /// Gets or sets the age of the person. You can only set this property on init.
+        /// </summary>
+        public int Age { get; init; }
+
+        /// <summary>
+        /// Gets or sets the sex of the person. You can only set this property on init.
+        /// </summary>
+        public Sex Sex { get; init; }
+    }
+}
